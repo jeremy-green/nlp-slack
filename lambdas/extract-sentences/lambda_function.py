d74e371eabd7304e676e14aeb8bf684a46d49631
Jeremy Green <hello@jeremy.green>
2020-08-23 08:19:26 -0400

    update extract-sentences lambda

--- lambdas/extract-sentences/lambda_function.py
+++ lambdas/extract-sentences/lambda_function.py
@@ -10,32 +10,19 @@ max_items = 25
 max_length = 4500
 
 def lambda_handler(event, context):
-    client = boto3.client('s3')
-    records = event['Records']
+    records = event['history']
+    messages = []
     for record in records:
-        s3 = record['s3']
-        bucket = s3['bucket']
-        object = s3['object']
-        name = bucket['name']
-        key = object['key']
-        response = client.get_object(
-            Bucket=name,
-            Key=key,
-        )
+        parsed_message = json.loads(record)
+        client_msg_id = parsed_message.get('client_msg_id')
+        ts = parsed_message.get('ts')
+        tokenized_sentences = nltk.sent_tokenize(parsed_message['text'])
+        filtered_sentences = list(set([sentence[:max_length] for sentence in tokenized_sentences if len(sentence.strip()) > 1]))[:max_items]
+        if client_msg_id is not None and len(filtered_sentences) > 0:
+            msg = {
+                'ts': ts,
+                'sentences': filtered_sentences
+            }
+            messages.append(msg)
 
-        messages = response['Body'].read().decode('utf-8').splitlines()
-        for message in messages:
-            parsed_message = json.loads(message)
-            client_msg_id = parsed_message.get('client_msg_id')
-            tokenized_sentences = nltk.sent_tokenize(parsed_message['text'])
-            filtered_sentences = list(set([sentence[:max_length] for sentence in tokenized_sentences if len(sentence.strip()) > 1]))[:max_items]
-            if client_msg_id is not None and len(filtered_sentences) > 0:
-                client.put_object(
-                    Body='\n'.join(filtered_sentences),
-                    Bucket=name,
-                    Key='{}/{}.txt'.format(os.getenv('S3_PREFIX'), client_msg_id),
-                )
-
-    return {
-        'statusCode': 204
-    }
+    return { 'history': messages }
