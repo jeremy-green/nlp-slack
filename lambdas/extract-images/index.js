8320ed34372a7ea370d9e2ce6e388fdec91e5866
Jeremy Green <hello@jeremy.green>
2020-08-23 16:34:38 -0400

    add debug code

--- lambdas/extract-images/index.js
+++ lambdas/extract-images/index.js
@@ -1,7 +1,9 @@
 exports.handler = async (event) => {
   const { history } = event;
+  console.log(history);
   const images = history.reduce((acc, curr) => {
     const { files = [] } = curr;
+    console.log(files);
     if (files.length) {
       return [...acc, ...files];
     }
