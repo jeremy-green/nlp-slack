b45008b94dd143dfacbab8f0e28b329858d3417f
Jeremy Green <hello@jeremy.green>
2020-08-25 16:33:19 -0400

    remove log statements

--- lambdas/get-sentiment/index.js
+++ lambdas/get-sentiment/index.js
@@ -4,8 +4,6 @@ const { Comprehend, DynamoDB } = require('aws-sdk');
 const { accessKeyId, secretAccessKey, region = 'us-east-1' } = require('config');
 const { generateDBObj } = require('@nlp-slack/helpers');
 
-console.log(generateDBObj.toString());
-
 const comprehend = new Comprehend({ region, accessKeyId, secretAccessKey });
 const dynamodb = new DynamoDB({ region, accessKeyId, secretAccessKey });
 
@@ -19,9 +17,6 @@ async function processHistory(item) {
   };
 
   const { ResultList: resultList } = await comprehend.batchDetectSentiment(params).promise();
-
-  console.log(resultList);
-  console.log(generateDBObj(resultList), '<======');
 
   const payload = {
     TableName: 'messages',
