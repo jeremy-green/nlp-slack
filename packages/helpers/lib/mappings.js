98cea2ce7ed5eaa9d518ce9d796359eaba1832b6
Jeremy Green <hello@jeremy.green>
2020-08-26 17:00:40 -0400

    add reversing dynamo code

--- packages/helpers/lib/mappings.js
+++ packages/helpers/lib/mappings.js
@@ -22,5 +22,31 @@ function mapDBProps(input) {
   return input;
 }
 
+function mapMessageProps(input) {
+  const [[key, val]] = Object.entries(input);
+  if (key === 'S') {
+    return val;
+  }
+
+  if (key === 'N') {
+    return +val;
+  }
+
+  if (key === 'L') {
+    return val.map((item) => mapDBProps(item));
+  }
+
+  if (key === 'M') {
+    return Object.entries(val).reduce((acc, [key, val]) => {
+      return { ...acc, [key]: mapDBProps(val) };
+    }, {});
+  }
+
+  return input;
+}
+
 exports.generateDBObj = (input) =>
   Object.entries(input).reduce((acc, [key, val]) => ({ ...acc, [key]: mapDBProps(val) }), {});
+
+exports.generateMessageObj = (input) =>
+  Object.entries(input).reduce((acc, [key, val]) => ({ ...acc, [key]: mapMessageProps(val) }), {});
