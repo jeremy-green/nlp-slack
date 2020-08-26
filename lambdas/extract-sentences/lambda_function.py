17a17e08dac2b5565c70fa724060a817a46494fc
Jeremy Green <hello@jeremy.green>
2020-08-26 13:49:08 -0400

    use black for formatting python

--- lambdas/extract-sentences/lambda_function.py
+++ lambdas/extract-sentences/lambda_function.py
@@ -1,32 +1,32 @@
 import json
 import nltk
 
-nltk.data.path.append('/tmp')
-nltk.download('punkt', download_dir='/tmp')
+nltk.data.path.append("/tmp")
+nltk.download("punkt", download_dir="/tmp")
 
 max_items = 25
 max_length = 4500
 
 
 def lambda_handler(event, context):
-    records = event['history']
+    records = event["history"]
     messages = []
     for record in records:
         parsed_message = json.loads(record)
-        client_msg_id = parsed_message.get('client_msg_id')
-        ts = parsed_message.get('ts')
-        tokenized_sentences = nltk.sent_tokenize(parsed_message['text'])
-        filtered_sentences = list(set([
-            sentence[:max_length]
-            for sentence
-            in tokenized_sentences
-            if len(sentence.strip()) > 1
-        ]))[:max_items]
+        client_msg_id = parsed_message.get("client_msg_id")
+        ts = parsed_message.get("ts")
+        tokenized_sentences = nltk.sent_tokenize(parsed_message["text"])
+        filtered_sentences = list(
+            set(
+                [
+                    sentence[:max_length]
+                    for sentence in tokenized_sentences
+                    if len(sentence.strip()) > 1
+                ]
+            )
+        )[:max_items]
         if client_msg_id is not None and len(filtered_sentences) > 0:
-            msg = {
-                'ts': ts,
-                'sentences': filtered_sentences
-            }
+            msg = {"ts": ts, "sentences": filtered_sentences}
             messages.append(msg)
 
-    return {'history': messages}
+    return {"history": messages}
