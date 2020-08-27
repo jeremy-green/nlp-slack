ae649e74cc5b6be453dc6ee195df6ffae79eeda5
Jeremy Green <hello@jeremy.green>
2020-08-26 21:16:19 -0400

    write file then write to dynamo

--- lambdas/format-history/index.js
+++ lambdas/format-history/index.js
@@ -20,19 +20,6 @@ function processHistory(item) {
     .promise();
 }
 
-async function createObject(message) {
-  const buffer = Buffer.from(JSON.stringify(message));
-  const Key = `${key}/${Date.now()}.${format}`;
-  const putObjectParams = {
-    Body: buffer,
-    Bucket: bucket,
-    Key,
-  };
-  console.log(putObjectParams);
-  await s3.putObject(putObjectParams).promise();
-  return { bucket, key: Key, arn: `arn:aws:s3:::${bucket}/${key}.${format}` };
-}
-
 exports.handler = async ({ bucket: Bucket, key, format }) => {
   const getObjectParams = {
     Key: `${key}.${format}`,
@@ -44,7 +31,22 @@ exports.handler = async ({ bucket: Bucket, key, format }) => {
   const { messages } = JSON.parse(payload);
 
   await Promise.all(messages.map((message) => limit(() => processHistory(message))));
-  const arns = await Promise.all(messages.map((message) => limit(() => createObject(message))));
+  const arns = await Promise.all(
+    messages.map((message) =>
+      limit(async () => {
+        const buffer = Buffer.from(JSON.stringify(message));
+        const Key = `${key}/${Date.now()}.${format}`;
+        const putObjectParams = {
+          Body: buffer,
+          Bucket: bucket,
+          Key,
+        };
+        console.log(putObjectParams);
+        await s3.putObject(putObjectParams).promise();
+        return { bucket, key: Key, arn: `arn:aws:s3:::${bucket}/${key}.${format}` };
+      }),
+    ),
+  );
 
   return arns;
 };
