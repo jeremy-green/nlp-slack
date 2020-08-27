a798f7ac953b7426e69fcec07b2f06e6555327d5
Jeremy Green <hello@jeremy.green>
2020-08-27 11:52:26 -0400

    cleanup stuff

--- lambdas/extract-sentences/lambda_function.py
+++ lambdas/extract-sentences/lambda_function.py
@@ -10,6 +10,20 @@ max_items = 25
 max_length = 4500
 
 
+def get_filtered_setences(text):
+    tokenized_sentences = nltk.sent_tokenize(text)
+    filtered_sentences = list(
+        set(
+            [
+                sentence[:max_length]
+                for sentence in tokenized_sentences
+                if len(sentence.strip()) > 1
+            ]
+        )
+    )[:max_items]
+    return filtered_sentences
+
+
 def lambda_handler(event, context):
     client = boto3.client("s3")
 
@@ -23,52 +37,40 @@ def lambda_handler(event, context):
     )
 
     messages = json.loads(response["Body"].read().decode("utf-8"))
-    print(messages)
     objects = []
     for message in messages:
-        print(message)
         client_msg_id = message.get("client_msg_id")
         ts = message.get("ts")
-        print(client_msg_id, ts)
-        if client_msg_id is not None:
-            tokenized_sentences = nltk.sent_tokenize(message["text"])
-            print(tokenized_sentences)
-            filtered_sentences = list(
-                set(
-                    [
-                        sentence[:max_length]
-                        for sentence in tokenized_sentences
-                        if len(sentence.strip()) > 1
-                    ]
-                )
-            )[:max_items]
-            print(filtered_sentences)
-            if len(filtered_sentences) > 0:
-                format = "txt"
-                s3_key = "{}/{}/{}.{}".format(
-                    os.getenv("S3_PREFIX"),
-                    s3_range,
-                    ts,
-                    format,
-                )
-                arn = "arn:aws:s3:::{}/{}".format(bucket, s3_key)
-                print("HERE")
-                client.put_object(
-                    Body="\n".join(filtered_sentences),
-                    Bucket=bucket,
-                    Key=s3_key,
-                )
+        filtered_sentences = get_filtered_setences(message["text"])
 
-                print("tHERE")
-                objects.append(
-                    {
-                        "bucket": bucket,
-                        "format": format,
-                        key: s3_key,
-                        range: s3_range,
-                        arn: arn,
-                    }
-                )
-                print("everywhere")
+        if client_msg_id is not None and len(filtered_sentences) > 0:
+            format = "txt"
+            s3_key = "{}/{}/{}.{}".format(
+                os.getenv("S3_PREFIX"),
+                s3_range,
+                ts,
+                format,
+            )
+            arn = "arn:aws:s3:::{}/{}".format(bucket, s3_key)
 
-    return {"objects": objects, "range": s3_range}
+            client.put_object(
+                Body="\n".join(filtered_sentences),
+                Bucket=bucket,
+                Key=s3_key,
+            )
+
+            objects.append(
+                {
+                    "bucket": bucket,
+                    "format": format,
+                    "key": s3_key,
+                    "range": s3_range,
+                    "arn": arn,
+                }
+            )
+
+    return {
+        "objects": objects,
+        "range": s3_range,
+        "bucket": bucket,
+    }
