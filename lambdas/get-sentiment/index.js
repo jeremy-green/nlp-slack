38826e5f79edc941b7acb79f697162e7902d7315
Jeremy Green <hello@jeremy.green>
2020-08-28 09:53:11 -0400

    save details to file

--- lambdas/get-sentiment/index.js
+++ lambdas/get-sentiment/index.js
@@ -1,4 +1,4 @@
-const { Comprehend } = require('aws-sdk');
+const { Comprehend, S3 } = require('aws-sdk');
 const {
   accessKeyId,
   secretAccessKey,
@@ -9,7 +9,10 @@ const {
 } = require('config');
 
 const comprehend = new Comprehend({ region, accessKeyId, secretAccessKey });
-exports.handler = async ({ results }) => {
+const s3 = new S3({ region, accessKeyId, secretAccessKey });
+exports.handler = async (event) => {
+  console.log(event);
+  const { results, TaskToken: taskToken } = event;
   const {
     Payload: { bucket: inputBucket, range, prefix: inputPrefix },
   } = results.find((result) => result.Payload.objects !== undefined);
@@ -20,11 +23,22 @@ exports.handler = async ({ results }) => {
       InputFormat: 'ONE_DOC_PER_LINE',
     },
     LanguageCode: 'en',
+    JobName: range,
     OutputDataConfig: {
       S3Uri: `s3://${outputBucket}/${outputPrefix}/${range}`,
     },
   };
   const result = await comprehend.startSentimentDetectionJob(params).promise();
   console.log(result);
+  const s = await s3
+    .putObject({
+      Body: Buffer.from(JSON.stringify({ ...result, taskToken })),
+      Bucket: outputBucket,
+      Key: `${outputPrefix}/${range}/event-details.json`,
+    })
+    .promise();
+
+  console.log(s);
+
   return result;
 };
