fe1bab817130a7b755bc854483f5129d43ebbf71
Jeremy Green <hello@jeremy.green>
2020-09-30 10:40:31 -0400

    update format-history function

--- lambdas/format-history/index.js
+++ lambdas/format-history/index.js
@@ -3,11 +3,11 @@ const pLimit = require('p-limit');
 
 const { DynamoDB, S3 } = require('aws-sdk');
 const { v4: uuid } = require('uuid');
-const { accessKeyId, secretAccessKey, region, prefix, bucket } = require('config');
+const { accessKeyId, secretAccessKey, region, prefix, bucket, endpoint, tableName } = require('config');
 const { generateDBObj } = require('@nlp-slack/helpers');
 
-const dynamodb = new DynamoDB({ region, accessKeyId, secretAccessKey });
-const s3 = new S3({ region, accessKeyId, secretAccessKey });
+const dynamodb = new DynamoDB({ region, accessKeyId, secretAccessKey, endpoint });
+const s3 = new S3({ region, accessKeyId, secretAccessKey, endpoint, s3ForcePathStyle: true });
 
 const limit = pLimit(1);
 
@@ -15,17 +15,19 @@ async function processHistory(item) {
   await new Promise((resolve) => setTimeout(() => resolve(), 500));
   return dynamodb
     .putItem({
-      TableName: 'messages',
+      TableName: tableName,
       Item: { ...generateDBObj(item), __appid: { S: uuid() } },
     })
     .promise();
 }
 
 exports.handler = async (event) => {
-  const { key, format, range, bucket: Bucket } = event;
+  console.log(event);
+
+  const { key, id } = event;
   const getObjectParams = {
-    Key: `${key}.${format}`,
-    Bucket,
+    Key: key,
+    Bucket: bucket,
   };
 
   const data = await s3.getObject(getObjectParams).promise();
@@ -34,10 +36,9 @@ exports.handler = async (event) => {
 
   await Promise.all(messages.map((message) => limit(() => processHistory(message))));
   const buffer = Buffer.from(JSON.stringify(messages));
-  const newFormat = 'json';
-  const newKey = `${prefix}/${range}/${Date.now()}.${newFormat}`;
+  const newKey = `${prefix}/${id}/${uuid()}`;
   const putObjectParams = { Body: buffer, Bucket: bucket, Key: newKey };
   await s3.putObject(putObjectParams).promise();
 
-  return { bucket, range, key: newKey, format: newFormat };
+  return { id, key: newKey };
 };
