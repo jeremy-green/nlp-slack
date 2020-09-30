952ab997d5d37b27910414e16179df46749551c4
Jeremy Green <hello@jeremy.green>
2020-09-30 10:32:28 -0400

    update channel-history function

--- lambdas/channel-history/index.js
+++ lambdas/channel-history/index.js
@@ -1,5 +1,6 @@
 const { S3 } = require('aws-sdk');
 const { WebClient } = require('@slack/web-api');
+const { v4: uuid } = require('uuid');
 
 const {
   botToken,
@@ -11,49 +12,47 @@ const {
   prefix,
   endpoint,
   fullHistory,
+  slackApiUrl,
 } = require('config');
 
-const botOptions = {};
+const botOptions = {
+  ...(slackApiUrl ? { slackApiUrl } : {}),
+};
+
 const web = new WebClient(botToken, botOptions);
-const s3 = new S3({ region, accessKeyId, secretAccessKey, endpoint });
+const s3 = new S3({ region, accessKeyId, secretAccessKey, endpoint, s3ForcePathStyle: true });
 
-const d = new Date();
-
-if (fullHistory === 'true') {
-  d.setFullYear(2020);
-  d.setMonth(0);
-  d.setDate(1);
-}
-
-d.setHours(0);
-d.setMinutes(0);
-d.setSeconds(0);
-d.setMilliseconds(0);
-
-const oldest = d.getTime() / 1000;
-const latest = Date.now() / 1000;
-
-const objects = [];
-async function saveMessages(content) {
-  const range = `${oldest}-${latest}`;
-  const key = `${prefix}/${range}/${Date.now()}`;
-  const format = 'txt';
-
+function saveHistory(path, content) {
+  const key = `${prefix}/${path}/${uuid()}`;
   const buffer = Buffer.from(JSON.stringify(content));
 
   const params = {
     Bucket: bucket,
-    Key: `${key}.${format}`,
+    Key: key,
     Body: buffer,
     ContentType: 'text/plain',
   };
 
-  await s3.putObject(params).promise();
-
-  return { bucket, key, format, range };
+  return s3.putObject(params).promise();
 }
 
-async function getMessages(cursor) {
+async function getHistory({ id, year, cursor }) {
+  const d = new Date();
+
+  if (fullHistory === 'true') {
+    d.setFullYear(year);
+    d.setMonth(0);
+    d.setDate(1);
+  }
+
+  d.setHours(0);
+  d.setMinutes(0);
+  d.setSeconds(0);
+  d.setMilliseconds(0);
+
+  const oldest = d.getTime() / 1000;
+  const latest = Date.now() / 1000;
+
   const opts = {
     limit: 100,
     inclusive: true,
@@ -64,17 +63,12 @@ async function getMessages(cursor) {
   };
 
   const history = await web.conversations.history(opts);
+  await saveHistory(id, history);
 
-  objects.push(await saveMessages(history));
+  const { has_more: hasMore, response_metadata: responseMetadata } = history;
+  const { next_cursor: nextCursor = null } = responseMetadata;
 
-  if (history.has_more) {
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-    await getMessages(history.response_metadata.next_cursor);
-  }
+  return { year, id, hasMore, cursor: nextCursor };
 }
 
-exports.handler = async () => {
-  await getMessages();
-
-  return { objects };
-};
+exports.handler = (event) => getHistory(event);
